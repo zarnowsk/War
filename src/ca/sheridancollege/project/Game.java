1a574f407b83cdb2f8d37b82b14602ccae5ea715
Michal <zarnowsk@sheridancollege.ca>
2019-07-21 19:25:17 -0400

    Added code for creating a user player with name and hot key entered by the user. Machine user gets created with default name and hot key. Blank scoreboard is initialized with player's and machine's names.

--- src/ca/sheridancollege/project/Game.java
+++ src/ca/sheridancollege/project/Game.java
@@ -10,11 +10,66 @@
  */
 package ca.sheridancollege.project;
 
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Game {
     
+    private static Scanner input = new Scanner(System.in);
+    
     public static void main(String[] args) {
         
+        Game game = new Game();
+        
+        //Create a new user
+        User player = game.createUser();
+        
+        //Create a machine user
+        User machine = new User("Machine", 'm');
+        machine.setMachine(true);
+        
+        //Create a blank scoreboard with user details
+        Scoreboard scoreboard = game.createScoreboard(player, machine);
+        
+    }
+    
+    /**
+     * Method creates and returns a new User entity from validated user input.
+     * @return A new User entity
+     */
+    private User createUser() {
+        
+        String userName = "";
+        String key = "";
+        
+        //Validate user input for player name. Must not be empty String
+        do {
+            System.out.println("Please enter your name: ");
+            userName = input.nextLine();
+        }while(userName.length() == 0);
+        
+        //Validate user input for hot key. Must be of length 1.
+        do {
+            System.out.println("Select your default 'play card' key: ");
+            key = input.nextLine();
+            if(key.length() != 1) {
+                System.out.println("Your play key must be a single character");
+            }
+        }while(key.length() != 1);
+        
+        char hotKey = key.charAt(0);
+        
+        User player = new User(userName, hotKey);
+        return player;
+    }
+
+    private Scoreboard createScoreboard(User player, User machine) {
+        //Player's name
+        String pName = player.getName();
+        //Machine's name
+        String mName = machine.getName();
+
+        Scoreboard scoreboard = new Scoreboard(pName, mName);
+
+        return scoreboard;
     }
 }
