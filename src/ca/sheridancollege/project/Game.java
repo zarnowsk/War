1bdbd38ea2b8b8d1cbc959edfd354fe690d6cef7
Michal <zarnowsk@sheridancollege.ca>
2019-07-21 18:43:57 -0400

    Base code for the main class with main method

--- src/ca/sheridancollege/project/Game.java
+++ src/ca/sheridancollege/project/Game.java
@@ -12,58 +12,9 @@ package ca.sheridancollege.project;
 
 import java.util.ArrayList;
 
-/**
- * The class that models your game. You should create a more specific
- * child of this class and instantiate the methods given.
- * @author dancye, 2018
- */
-public abstract class Game 
-{
-    private final String gameName;//the title of the game
-    private ArrayList <Player> players;// the players of the game
+public class Game {
     
-    public Game(String givenName)
-    {
-        gameName = givenName;
-        players = new ArrayList();
+    public static void main(String[] args) {
+        
     }
-
-    /**
-     * @return the gameName
-     */
-    public String getGameName() 
-    {
-        return gameName;
-    }
-    
-     /**
-     * @return the players of this game
-     */
-    public ArrayList <Player> getPlayers() 
-    {
-        return players;
-    }
-
-    /**
-     * @param players the players of this game
-     */
-    public void setPlayers(ArrayList <Player> players) 
-    {
-        this.players = players;
-    }
-    
-    /**
-     * Play the game. This might be one method or many method calls depending
-     * on your game.
-     */
-    public abstract void play();
-    
-    /**
-     * When the game is over, use this method to declare and display a winning
-     * player.
-     */
-    public abstract void declareWinner();
-
-   
-    
-}//end class
+}
