9d63e912d1bf02878f55dfe790f0d3f00d9987b2
Michal <zarnowsk@sheridancollege.ca>
2019-08-09 22:23:38 -0400

    Updated comments

--- src/ca/sheridancollege/project/Deck.java
+++ src/ca/sheridancollege/project/Deck.java
@@ -4,19 +4,22 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 /**
- *
+ * Class representing a deck of cards. Multiple Decks are used for game play.
  * @author Paige Freeman-Cyopeck
  */
 class Deck {
     
     private ArrayList<Card> deck = new ArrayList<>();
     
+    /**
+     * Default constructor
+     */
     public Deck() {
         
     }
     
     /**
-     * Method which generates the card deck.
+     * Method which generates the card deck with Enum Suit and Value values.
      * @return Returns an ArrayList<Card> which has been generated according to
      * a standard card deck (52 cards, 13 cards for each suit.)
      */
@@ -30,14 +33,27 @@ class Deck {
         return deck;
     }
 
+    /**
+     * Method used to retrieve a Card from the Deck based on its position in the deck.
+     * @param index Deck position of required Card
+     * @return Required Card
+     */
     public Card get(int index) {
         return deck.get(index);
     }
     
+    /**
+     * Method used to add a Card to the Deck
+     * @param card to be added to the Deck
+     */
     public void add(Card card) {
         deck.add(card);
     }
     
+    /**
+     * Method used to return the "top" card in the Deck
+     * @return "top" Card
+     */
     public Card pop() {
         if(deck.size() > 0) {
             Card card = deck.get(0);
@@ -59,14 +75,16 @@ class Deck {
     }
     
     /**
-     * Currently checks the size of the deck as it stands.
-     * @param deck
+     * Method currently checks the size of the deck as it stands.
      * @return the current size of the deck.
      */
     public int checkSize() {
         return deck.size();
     }
     
+    /**
+     * Method used to remove all Card in the Deck
+     */
     public void clear() {
         deck.clear();
     }
